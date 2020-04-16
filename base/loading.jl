5add326cf140a5bc68de852ff028a10b59fba018
Chris Foster <chris42f@gmail.com>
2020-04-16 12:20:55 +1000

    In errorshow UI, filter out frames for _include implementation
    This simplifies stack traces for include() which were made much more
    complex by moving jl_parse_eval_all to the Julia layer.

--- base/loading.jl
+++ base/loading.jl
@@ -1149,6 +1149,7 @@ Base.include # defined in Base.jl
 
 # Full include() implementation which is used after bootstrap
 function _include(mapexpr::Function, mod::Module, _path::AbstractString)
+    @_noinline_meta # Workaround for module availability in _simplify_include_frames
     path, prev = _include_dependency(mod, _path)
     for callback in include_callbacks # to preserve order, must come before Core.include
         invokelatest(callback, mod, path)
