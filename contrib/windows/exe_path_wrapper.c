644bdf0426613ecdaf1a42c7edda91492a87f822
Elliot Saba <staticfloat@gmail.com>
2020-04-20 15:29:05 -0700

    Fix relative windows pathing

--- contrib/windows/exe_path_wrapper.c
+++ contrib/windows/exe_path_wrapper.c
@@ -4,10 +4,14 @@
 #include <shlwapi.h>
 #define ENVVAR_MAXLEN 32760
 
-/* PATH_ENTRIES is our simulated RPATH, usually of the form "../path1;../path2;../path3" */
+/* PATH_ENTRIES is our simulated RPATH, usually of the form 'TEXT("../path1"), TEXT("../path2"), ...' */
 #ifndef PATH_ENTRIES
 #define PATH_ENTRIES  TEXT("")
 #endif
+
+LPWSTR pathEntries[] = {
+    PATH_ENTRIES
+};
 
 /* JULIA_EXE_PATH is the relative path to julia.exe */
 #ifndef JULIA_EXE_PATH
@@ -25,21 +29,33 @@ int wmain(int argc, wchar_t *argv[], wchar_t *envp[]) {
     // On windows, we simulate RPATH by pushing onto PATH
     LPWSTR pathVal = (LPWSTR) malloc(ENVVAR_MAXLEN*sizeof(WCHAR));
     DWORD dwRet = GetEnvironmentVariable(TEXT("PATH"), pathVal, ENVVAR_MAXLEN);
+    DWORD numPathEntries = sizeof(pathEntries)/sizeof(pathEntries[0]);
     if (dwRet == 0) {
         // If we cannot get PATH, then our job is easy!
         pathVal[0] = '\0';
     } else {
         // Otherwise, we append, if we have enough space to:
-        if (ENVVAR_MAXLEN - dwRet < _tcslen(PATH_ENTRIES) ) {
-            printf("ERROR: Cannot append entries to PATH: not enough space in environment block.  Reduce size of PATH!");
+        DWORD currFileDirLen = wcslen(currFileDir);
+        DWORD totalPathLen = dwRet + 1 + currFileDirLen;
+        for (int env_idx=0; env_idx<numPathEntries; ++env_idx) {
+            totalPathLen += 1 + currFileDirLen + 1 + wcslen(pathEntries[env_idx]);
+        }
+        if (ENVVAR_MAXLEN < totalPathLen ) {
+            fprintf(stderr, "ERROR: Cannot append entries to PATH: not enough space in environment block.  Reduce size of PATH!");
             exit(1);
         }
         lstrcat(pathVal, TEXT(";"));
     }
     // We always add the current directory (e.g. `bin/`) to PATH so that we can find e.g. libjulia.dll
     lstrcat(pathVal, currFileDir);
-    lstrcat(pathVal, TEXT(";"));
-    lstrcat(pathVal, TEXT(PATH_ENTRIES));
+
+    // For each entry in PATH_ENTRIES, tack it on to the end, relative to the current directory:
+    for (int env_idx=0; env_idx<numPathEntries; ++env_idx) {
+        lstrcat(pathVal, TEXT(";"));
+        lstrcat(pathVal, currFileDir);
+        lstrcat(pathVal, TEXT("\\"));
+        lstrcat(pathVal, pathEntries[env_idx]);
+    }
     SetEnvironmentVariable(TEXT("PATH"), pathVal);
     free(pathVal);
 
